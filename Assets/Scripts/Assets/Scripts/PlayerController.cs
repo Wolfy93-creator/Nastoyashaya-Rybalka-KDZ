041042aa42140e2cd67e166fbb0f26d36d3199b9
Wolfy93-creator <dmitrishina75@gmail.com>
2025-04-19 21:00:23 +0300

    Update PlayerController.cs

--- Assets/Scripts/Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/Assets/Scripts/PlayerController.cs
@@ -1,1 +1,32 @@
-Assets/Scripts/PlayerController.cs
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 100f;
+    private CharacterController controller;
+    private float verticalRotation = 0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
